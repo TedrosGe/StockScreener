4075776e7cc3f20be336ef55ccce7c4591abc51b
teddy <tedros.gebrenegus@gmail.com>
2023-06-17 20:32:47 -0400

    frontend added: /lookup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App</h1>
+      <p>Start building your amazing application!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import Components from './Components'
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Components/>
+    
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
